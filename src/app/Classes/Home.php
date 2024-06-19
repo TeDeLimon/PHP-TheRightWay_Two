e108bbbb2232d40ee8875df2da5da830782bee87
TeDeLimon <tysonpopluis@gmail.com>
2024-06-19 23:15:48 +0200

    Ending the lesson

--- src/app/Classes/Home.php
+++ src/app/Classes/Home.php
@@ -6,6 +6,26 @@ class Home
 {
     public function index()
     {
-        return 'Home Page';
+        // $_SESSION is a superglobal array that stores session data.
+        // $_SESSION count doesn't exist, so we need to initialize it to 0.
+        $_SESSION['count'] = ($_SESSION['count'] ?? 0) + 1;
+
+        return 'Home Page. You have visited this page ' . $_SESSION['count'] . ' times.';
+    }
+
+    public function cookies()
+    {
+
+        // Cookies must be set before any output is sent to the browser.
+
+        setcookie(
+            name: 'userName',
+            value: 'Luis Villanueva',
+            expires_or_options : time() + 3600
+        );
+
+        // debug($_COOKIE);
+
+        return 'Cookies Page.';
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -10,20 +10,31 @@ use App\Router\Router;
 use App\Classes\Home;
 use App\Classes\Invoice;
 
-//? REQUEST can be from multiple sources
+//? Cookies are disabled by default in the browser, so we need to enable them.
 
-// Frontend (Javascript), Backend (PHP), Mobile (Java, Kotlin, Swift), Desktop (C++, C#), IoT (C, Python), Mobile App (React Native, Flutter), Forms...
+// Cookies are stored in client's browser and are sent with every request to the server.
+// Cookies are destroyed when the expiration time is reached or when the user deletes them.
 
-//? When we should use GET Request? When we want to get data from the server (Fetching data from the server)
+//? Session is stored on the server and a session id is sent to the client's browser.
+// The session id is used to identify the session data on the server.
+// Session data is destroyed when the session is destroyed or when the session expires.
 
-//? When we should use POST Request? When we want to send data to the server, like submitting a form
+// This will start the session and create a session id if it doesn't exist.
+session_start();
 
 $router = new Router();
 
 $router
     ->get('/', [Home::class, 'index'])
+    ->get('/cookies', [Home::class, 'cookies'])
     ->get('/invoices', [Invoice::class, 'index'])
     ->get('/invoices/create', [Invoice::class, 'create'])
     ->post('/invoices/create', [Invoice::class, 'store']);
 
 echo $router->resolve($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+
+// phpinfo();
+
+// Output buffering is turned on by default in PHP. It's a mechanism that allows you to store the output of a script in a buffer and send it to the browser all at once.
+
+// When headers are sent to the browser, you can't set cookies or session data and no longer can send more headers or modify the existing ones.
